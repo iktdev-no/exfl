d182f85d7097e9532ae705fe920fad2f919e5d01
Brage <bskjon@outlook.com>
2023-08-03 00:17:36 +0200

    Update

--- src/main/kotlin/no/iktdev/exfl/time/TimeUtil.kt
+++ src/main/kotlin/no/iktdev/exfl/time/TimeUtil.kt
@@ -6,4 +6,20 @@ class TimeUtil {
             return time * 1000L
         }
     }
+
+    class UnixTime {
+        companion object {
+            fun now() = System.currentTimeMillis() / 1000L
+
+            fun reduceDays(time: Int, days: Int): Int {
+                return time - (days * 86400)
+            }
+
+            fun reduceHours(time: Int, hours: Int): Int {
+                return time - (hours * 3600)
+            }
+        }
+
+    }
+
 }
--- src/main/kotlin/no/iktdev/exfl/time/UnixTime.kt
+++
@@ -1,9 +0,0 @@
-package no.iktdev.exfl.time
-
-import no.iktdev.extender.time.UnixTimestamp
-
-class UnixTime {
-    companion object {
-        fun now() = UnixTimestamp(System.currentTimeMillis() / 1000L)
-    }
-}
